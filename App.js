dd4e6f1cdd784453b9fc32e66b52325ad48cc5e3
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2020-05-02 15:58:08 +0000

    init commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './Components/Home';
+import AddToCart from './Components/AddToCart';
+import PaymentSuccessful from './Components/PaymentSuccessful'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Home/> */}
+      {/* <AddToCart/> */}
+      <PaymentSuccessful/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 32,
+    marginTop: 65,
+    backgroundColor: '#fff',
+    
+  },
+});
---
+++ Components/AddToCart.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+
+export default class AddToCart extends Component {
+    render() {
+        return (
+            <View style={styles.homeScreenContainer}>
+                    <Text style={styles.headingText}>ADD TO CART</Text>
+                <View>
+                    <Text style={styles.paragraphText}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Ducimus odit assumenda officia. Quam eos nam, alias possimus aperiam,
+                        est voluptatum ipsam tempore quo dolor ab delectus repudiandae officia,
+                        in quisquam?
+                    </Text>
+                </View>
+                <View>
+                    <Image source={require('./../assets/images/add_to_cart.png')} style={styles.image}/>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Next</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.swiperContainer}>
+                    <TouchableOpacity style={styles.swiper}></TouchableOpacity>
+                    <TouchableOpacity style={[styles.swiper, styles.swiperHover]}></TouchableOpacity>
+                    <TouchableOpacity style={styles.swiper}></TouchableOpacity>
+                    <Text style={styles.previous}>Previous</Text>
+                    <Text style={styles.skipText}>Skip</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    homeScreenContainer:{},
+    headingText:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginVertical: 10
+    },
+    paragraphText:{
+        color: '#b1bab3',
+
+    },
+    image:{
+        width: 220,
+        height: 250,
+        alignSelf: 'center',
+        marginVertical: 15
+    },
+    button:{
+        width: 150,
+        height: 50,
+        borderRadius: 30,
+        alignSelf: 'center',
+        backgroundColor: '#8470c4',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#fff'
+    },
+    swiperContainer:{
+        flexDirection: 'row',
+        position:'absolute',
+        top: 540,
+        alignSelf: "center"
+    },
+    swiper:{
+        backgroundColor: '#b6a5cc',
+        height: 7,
+        width:7,
+        borderRadius: 40,
+        marginHorizontal: 1
+    },
+    swiperHover:{
+        backgroundColor: '#735d8f',
+        width: 14
+    },
+    skipText:{
+        position:'absolute',
+        left: 160,
+        bottom: -10,
+        color: '#b8b2bf'
+    },
+    previous:{
+        position: 'absolute',
+        left: -140,
+        bottom: -10,
+        color: '#b8b2bf'
+    }
+})
